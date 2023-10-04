aef7390c56b2053e64e9cfa952fe2cfd98c6c7cf
mtthdz <matt@mtthdz.com>
2023-10-03 23:01:48 -0400

    footer component

---
+++ components/Footer/Footer.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterStyles = styled.section`
+  margin-top: 20px;
+  text-align: right;
+
+  a {
+    margin-right: 12px;
+  }
+
+  a:last-child {
+    margin: 0;
+  }
+`;
---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { FooterStyles } from "./Footer.styles";
+
+const Footer = () => {
+  return (
+    <FooterStyles>
+      <a href="https://github.com/mtthdz" target="_blank">github</a>
+      <a href="https://linkedin.com/in/mtthdz" target="_blank">linkedin</a>
+      <a href="mailto:matt@mtthdz.com" target="_blank">email</a>
+    </FooterStyles>
+  )
+}
+
+export default Footer;
