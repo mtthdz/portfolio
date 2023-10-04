9eb127b4bc9cf98599c026c058df7707bfa00baa
mtthdz <matt@mtthdz.com>
2023-10-03 23:01:38 -0400

    main body component

---
+++ components/Body/Body.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SectionStyles = styled.section`
+  h1 {
+    font-size: 1.4rem;
+    font-weight: 300;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 1.4rem;
+    margin-bottom: 20px;
+  }
+`
---
+++ components/Body/index.tsx
@@ -0,0 +1,13 @@
+import { SectionStyles } from "./Body.styles"
+
+const Body = () => {
+  return (
+    <SectionStyles>
+      <h1>I&apos;m Matt Hernandez</h1>
+      <p>I&apos;m a full stack developer based in Toronto. I&apos;m currently co-creating rax, an app akin to airbnb for fashion, where I&apos;m solving the problem of MVP execution and scale. I also lead app development on a small agency team, being instrumental in the process from ideation and through into execution and iteration. Leadership roles take up the majority of my 3 years of experience because of my ability to find common ground between people and the work we do.</p>
+      <p>I lean towards the front end out of my love of design, but my experience building out backends and my previous career in finance demonstrate both my curiosity and my familiarity with navigating through big data and abstract complexities.</p>
+    </SectionStyles>
+  )
+}
+
+export default Body;
