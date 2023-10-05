9c01ea69bdbe82b20a860ebaee3a81b8caf715b2
mtthdz <matt@mtthdz.com>
2023-10-04 21:10:37 -0400

    directory and markup refactor to home

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,20 +1,15 @@
 import BlogList from "@/components/BlogList";
 import Body from "@/components/Body";
 import Footer from "@/components/Footer";
-import styled from "styled-components"
 
 const DirectoryPage = () => {
   return (
-    <DirectoryStyles>
+    <section>
       <Body />
       <BlogList />
       <Footer />
-    </DirectoryStyles>
+    </section>
   )
 }
 
 export default DirectoryPage;
-
-const DirectoryStyles = styled.div`
-  /* margin-top: 50px; */
-`;
