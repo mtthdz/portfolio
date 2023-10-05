c0090b63b4a3257c5a74d64ff6e7935a2981fcea
mtthdz <matt@mtthdz.com>
2023-10-04 21:29:58 -0400

    added head element to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import { GlobalStyles } from '@/components/styles/GlobalStyles'
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"; 
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
+      <Head>
+        <title>Matt Hernandez</title>
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </Layout>
