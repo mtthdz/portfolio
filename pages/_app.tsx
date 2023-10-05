280f24b885de5bd570d8da1394634f882b55ea77
mtthdz <matt@mtthdz.com>
2023-10-04 21:51:47 -0400

    removed fontawesome code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from '@/components/styles/GlobalStyles'
-import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core"; 
 import Layout from '@/components/Layout';
 import Head from 'next/head';
-
-config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
