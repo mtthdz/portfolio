b1f1a90f6d6d65ee897236b716f83ac47de730ef
mtthdz <matt@mtthdz.com>
2023-10-04 22:17:08 -0400

    spacing correction in some text

--- components/Body/index.tsx
+++ components/Body/index.tsx
@@ -4,7 +4,7 @@ const Body = () => {
   return (
     <SectionStyles>
       <h1>I&apos;m Matt Hernandez</h1>
-      <p>I&apos;m a full stack developer based in Toronto. I&apos;m currently co-creating rax, an app akin to Airbnb for fashion, where I&apos;m solving the problem of MVP execution and scale. I also lead app development on a small agency team, being instrumental in the ideation, execution, and iteration of ourframework and its products. Leadership roles take up the majority of my 3 years of experience because of my ability to find common ground between people and the work we do.</p>
+      <p>I&apos;m a full stack developer based in Toronto. I&apos;m currently co-creating rax, an app akin to Airbnb for fashion, where I&apos;m solving the problem of MVP execution and scale. I also lead app development on a small agency team, being instrumental in the ideation, execution, and iteration of our framework and its products. Leadership roles take up the majority of my 3 years of experience because of my ability to find common ground between people and the work we do.</p>
       <p>My main proficiencies are TypeScript and React. I lean towards the front end out of my love of design, but my backend experience and my previous career in finance demonstrate my curiosity and familiarity with tackling big data and abstract complexities.</p>
     </SectionStyles>
   )
